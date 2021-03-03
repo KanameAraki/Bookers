1693d39071cad5c1397053187d05cbefe6eb4984
EC2 Default User <ec2-user@ip-172-31-69-159.ec2.internal>
2021-03-03 08:09:30 +0000

    [Finish]Second-Task

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,5 @@
 class BooksController < ApplicationController
 
-  def top
-  end
-
   def index
     @book = Book.new
     @books = Book.all
@@ -40,7 +37,7 @@ class BooksController < ApplicationController
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to books_path
+    redirect_to books_path,notice:"successfully"
   end
 
   private
